ef082355e18e3e92a9e20b7877f9c22e3611df1d
mgonzalez12 <ing.marcosgonzalez1@gmail.com>
2018-07-24 21:58:00 -0500

    reactjs-practica2

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 // Dependencies
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 // Components
 import Header from './Global/Header';
@@ -10,11 +11,15 @@ import Footer from './Global/Footer';
 import items from '../data/menu';
 
 class App extends Component {
+  static propTypes = {
+    children: PropTypes.object.isRequired
+  };
   render() {
+    const { children } = this.props;
     return (
       <div className="App">
        <Header title="practica" items={items} />
-        <Content />
+        <Content body={children} />
         <Footer  /*copyright="&copy; React 2018."*/ />
       </div>
     );
--- src/components/Global/Content.js
+++ src/components/Global/Content.js
@@ -1,85 +1,19 @@
+// Dependencies
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// Assets
 import './css/Content.css';
 
 class Content extends Component {
-    constructor(){
-        super();
-
-        this.state = {
-            count: 0,
-            number1: 0,
-            number2: 0,
-            result: 0
-        };
-
-        this.handleCountClick = this.handleCountClick.bind(this);
-        this.handleResultClick = this.handleResultClick.bind(this);
-        this.handleInputChanged = this.handleInputChanged.bind(this);
-    }
-
-    // sirve para saber si ya se cargo el componente
-    componentDidMount(){
-        this.setState({
-            count: 1
-        });
-    }
-
-    // function to handle the click
-    handleCountClick(e){
-        if (e.target.id == "add") {
-            this.setState({
-                count: this.state.count + 1
-            });
-        } else if (e.target.id === "subtract" && this.state.count > 0) {
-            this.setState({
-                count: this.state.count - 1
-            })
-        } else{
-            this.setState({
-                count: 0
-            })
-        }
-    }
-
-    // funcion
-    handleResultClick(e){
-        this.setState({
-            result: this.state.number1 + this.state.number2
-        });
-    }
-
-    handleInputChanged(e){
-        if (e.target.id === 'number1') {
-            this.setState({
-                number1:Number(e.target.value)
-            });
-        }else{
-            this.setState({
-                number2: Number(e.target.value)
-            });
-        }
-    }
-
+    static propTypes = {
+        body: PropTypes.object.isRequired
+    };
     render() {
-        console.log('Ejecuta render()');
+        const { body } = this.props;
         return (
             <div className="Content">
-              <h1> Counter: {this.state.count}</h1> 
-                <p> 
-                    <button id="add" onClick={this.handleCountClick}> + </button>
-                    <button id="subtract" onClick= {this.handleCountClick}> - </button>
-                    <button id="reset" onClick= {this.handleCountClick}> Reset </button>
-                </p>
-
-                <h2> Calculador </h2>
-                <p>
-                    <input id="number1" type="number" value={this.state.number1} onChange={this.handleInputChanged}/>+
-                    <input id="number2" type="number" value={this.state.number2} onChange= {this.handleInputChanged} />
-
-                    <button id="result" onClick= {this.handleResultClick} >Result </button> 
-
-                    <p>{this.state.result} </p>   
-                </p>
+            {body}
             </div>
         );
     }
